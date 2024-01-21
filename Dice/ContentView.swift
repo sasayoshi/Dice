6e30e0a198bef886c370443fd642b580021ed866
Yoshihiro Sasago <sasayoshi.0605@gmail.com>
2024-01-21 18:47:07 +0900

    Diceアプリ作成

--- Dice/ContentView.swift
+++ Dice/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomNumber = 1
+    @State private var timer: Timer?
+    @State private var isRolling = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Image(systemName: "die.face.\(randomNumber)")
+                .resizable()
+                .scaledToFit()
+                .frame(width: UIScreen.main.bounds.width/2)
+                .padding()
+            Spacer()
+            Button {
+                playDice()
+            } label: {
+                Text("サイコロを振る")
+                    .padding()
+                    .background(Color.orange)
+                    .foregroundColor(.black)
+                    .cornerRadius(10)
+            }
+            .disabled(isRolling)
+            Spacer()
         }
         .padding()
+    }
+    
+    private func playDice(){
+        print("ボタンが押されたよ")
+        isRolling = true
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true){_  in
+            randomNumber = Int.random(in: 1...6)
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
+            timer?.invalidate()
+            timer = nil
+            isRolling = false
+        }
     }
 }
 
