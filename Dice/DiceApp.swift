f56c687f9bb5a12cd5564cadff93020601003be8
Yoshihiro Sasago <sasayoshi.0605@gmail.com>
2024-01-21 18:18:12 +0900

    Initial Commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by sasayoshi on 2024/01/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
